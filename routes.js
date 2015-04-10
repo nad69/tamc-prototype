9653f29d86b52dd9a1997c62c1457ea9c7f38193
unknown <andrew.j.charlton@accenture.com>
2015-04-10 14:48:46 +0100

    Prototype Version 6 - AJC

--- routes.js
+++ routes.js
@@ -479,7 +479,64 @@ app.get('/app-calc1_4', function (req, res) {
             {'assetPath' : assetPath });
       });
 
-                  
+  /**9 April 2015 V.5**/
+            app.get('/app-start1_6', function (req, res) {
+          res.render('tamc-app/app-start1_6', 
+            {'assetPath' : assetPath });
+      });  
+      app.get('/app-calc1_6', function (req, res) {
+          res.render('tamc-app/app-calc1_6', 
+            {'assetPath' : assetPath });
+      });
+            app.get('/app-ida-pro6', function (req, res) {
+          res.render('tamc-app/app-ida-pro6', 
+            {'assetPath' : assetPath });
+      });   
+                  app.get('/app-ida1v4-pro6', function (req, res) {
+          res.render('tamc-app/app-ida1v4-pro6', 
+            {'assetPath' : assetPath });
+      }); 
+                        app.get('/app-exp1v4-pro6', function (req, res) {
+          res.render('tamc-app/app-exp1v4-pro6', 
+            {'assetPath' : assetPath });
+      }); 
+                
+           app.get('/app-calc-benefit', function (req, res) {
+          res.render('tamc-app/app-calc-benefit', 
+            {'assetPath' : assetPath });
+      });
+                        app.get('/app-calc-notbenefit', function (req, res) {
+          res.render('tamc-app/app-calc-notbenefit', 
+            {'assetPath' : assetPath });
+      });
+                  app.get('/app-calc-noteligible', function (req, res) {
+          res.render('tamc-app/app-calc-noteligible', 
+            {'assetPath' : assetPath });
+      });    
+             app.get('/app-verify1_6', function (req, res) {
+          res.render('tamc-app/app-verify1_6', 
+            {'assetPath' : assetPath });
+      });
+             app.get('/app-form1_6', function (req, res) {
+          res.render('tamc-app/app-form1_6', 
+            {'assetPath' : assetPath });
+      }); 
+      app.get('/app-confirm1_6', function (req, res) {
+          res.render('tamc-app/app-confirm1_6', 
+            {'assetPath' : assetPath });
+      });
+       app.get('/app-finished1_6', function (req, res) {
+          res.render('tamc-app/app-finished1_6', 
+            {'assetPath' : assetPath });
+      });  
+                   app.get('/emailv6', function (req, res) {
+          res.render('tamc-app/emailv6', 
+            {'assetPath' : assetPath });
+      });
+            app.get('/email_alert6', function (req, res) {
+          res.render('tamc-app/email_alert6', 
+            {'assetPath' : assetPath });
+      });                
 
 /**IDA Paths AJC 16/12/14**/
       app.get('/app-ida1', function (req, res) {
